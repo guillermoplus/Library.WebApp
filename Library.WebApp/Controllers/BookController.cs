40e017316a5d37d6fd0028f9bf669a0ffbc04fdd
guillermoplus <luisgal93@hotmail.com>
2025-01-27 23:41:53 -0500

    add authorization to BookController and update layout for authenticated users

--- Library.WebApp/Controllers/BookController.cs
+++ Library.WebApp/Controllers/BookController.cs
@@ -1,9 +1,11 @@
 using Library.Application.Interfaces;
 using Library.Application.UseCases;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.WebApp.Controllers;
 
+[Authorize]
 public class BookController : Controller
 {
     public readonly ILogger<BookController> _logger;
